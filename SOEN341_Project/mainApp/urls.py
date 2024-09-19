6ca542daacdfb81bc45a54f3463afb0d91fc7f9d
Transyfor <40695080+Transyfor@users.noreply.github.com>
2024-09-19 18:54:16 -0400

    fixed home view error

--- SOEN341_Project/mainApp/urls.py
+++ SOEN341_Project/mainApp/urls.py
@@ -7,5 +7,5 @@ from .views import *
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
-    path('home/', home_view, name='home'), 
+    #path('home/', home_view, name='home'), 
 ]
